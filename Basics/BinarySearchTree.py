7b6dfe6a8785998e1c7a1697a2e797622779807f
stephancmorris <stephancmorris@gmail.com>
2023-11-25 21:09:10 +1100

    Designing Binary Search Tree

--- Basics/BinarySearchTree.py
+++ Basics/BinarySearchTree.py
@@ -1,51 +1,115 @@
 class TreeNode:
-    def __init__(self, key):
-        self.left = None
-        self.right = None
-        self.val = key
+    def __init__(self, key: int, val: int):
+        self.key = key        # The key used for sorting in the BST
+        self.val = val        # The value associated with the key
+        self.left = None      # Pointer to left child
+        self.right = None     # Pointer to right child
 
-class BinarySearchTree:
+#This class implements a binary search tree map using the TreeNode class.
+class TreeMap:
     def __init__(self):
-        self.root = None
+        self.root = None  # Root of the BST
 
-    def insert(self, key):
-        if self.root is None:  
-            self.root - TreeNode(key)
-        else:
-            self._insert_recursive(self.root, key)
 
-    def _insert_recursive(self, current_node, key):
-        if key < current_node.val:
-            if current_node.left is None:
-                current_node.left = TreeNode(key)
+#Inserts a new key-value pair into the BST.
+    def insert(self, key: int, val: int) -> None:
+        # Create a new TreeNode
+        newNode = TreeNode(key, val)
+        # If the tree is empty, set the new node as root
+        if self.root == None:
+            self.root = newNode
+            return
+
+    # Traverse the tree to find the insertion point
+        current = self.root
+        while True:
+            # Go left if key is smaller than current key
+            if key < current.key:
+                if current.left == None:
+                    current.left = newNode
+                    return
+                current = current.left
+            # Go right if key is greater than current key
+            elif key > current.key:
+                if current.right == None:
+                    current.right = newNode
+                    return
+                current = current.right
+            # Update the value if key already exists
             else:
-                self._insert_recursive(current_node.left, key)
-        elif key > current_node.val:
-            if current_node.right is None:
-                current_node.right = TreeNode(key)
+                current.val = val
+                return
+
+#Retrieves the value for a given key.
+    def get(self, key: int) -> int:
+        current = self.root
+        # Traverse the tree to find the key
+        while current != None:
+            if key < current.key:
+                current = current.left
+            elif key > current.key:
+                current = current.right
             else:
-                self._insert_recursive(current_node.right, key)
+                return current.val
+        return -1  # Return -1 if key not found
 
-    def search(self, key):
-        return self._search_recursive(self.root, key)
-    
-    def _search_recursive(self, current_node, key):
-        if current_node is None:
-            return False #Key is not found
-        if key == current_node.val:
-            return True #Key found
-        elif key < current_node.val:
-            return self._search_recursive(current_node.left, key)
+#Returns the minimum value in the BST.
+    def getMin(self) -> int:
+        current = self.findMin(self.root)
+        return current.val if current else -1
+
+#Finds the node with the minimum key.    
+    def findMin(self, node: TreeNode) -> TreeNode:
+    # Traverse to the leftmost node
+        while node and node.left:
+            node = node.left
+        return node
+
+#Returns the maximum value in the BST.
+    def getMax(self) -> int:
+        current = self.root
+        # Traverse to the rightmost node
+        while current and current.right:
+            current = current.right
+        return current.val if current else -1
+
+#Removes a key-value pair from the BST.
+    def remove(self, key: int) -> None:
+        self.root = self.removeHelper(self.root, key)
+
+#A recursive helper function for removing a node.
+    def removeHelper(self, curr: TreeNode, key: int) -> TreeNode:
+        # Base case: Node is not found
+        if curr == None:
+            return None
+
+        # Recursive case: Traverse to find the node
+        if key > curr.key:
+            curr.right = self.removeHelper(curr.right, key)
+        elif key < curr.key:
+            curr.left = self.removeHelper(curr.left, key)
         else:
-            return self._search_recursive(current_node.right, key)
+            # Node with only one child or no child
+            if curr.left == None:
+                return curr.right
+            elif curr.right == None:
+                return curr.left
+            # Node with two children: Get the inorder successor
+            minNode = self.findMin(curr.right)
+            curr.key = minNode.key
+            curr.val = minNode.val
+            curr.right = self.removeHelper(curr.right, minNode.key)
+        return curr
 
-# Example usage:
-bst = BinarySearchTree()
-bst.insert(3)
-bst.insert(1)
-bst.insert(4)
-bst.insert(2)
+#Returns a list of keys in inorder traversal.
+    def getInorderKeys(self) -> List[int]:
+        result = []
+        self.inorderTraversal(self.root, result)
+        return result
 
-# Search for a value
-print(bst.search(4))  # Output: True
-print(bst.search(5))  # Output: False
+#A recursive helper function for inorder traversal.
+    def inorderTraversal(self, root: TreeNode, result: List[int]) -> None:
+        if root != None:
+            self.inorderTraversal(root.left, result)
+            result.append(root.key)
+            self.inorderTraversal(root.right, result)
--- Basics/LinkedList.py
+++ Basics/LinkedList.py
@@ -1,57 +1,64 @@
 class ListNode:
     def __init__(self, val, next_node=None):
-        self.val = val
-        self.next = next_node
+        self.val = val          # The value stored in the node
+        self.next = next_node   # The reference to the next node; defaults to None
+
+
 
 class LinkedList:
-    
     def __init__(self):
-        #Having a node mades this easier for edge cases
-        self.head = ListNode(-1)
-        self.tail = self.head
+        # Initialize with a dummy head node
+        self.head = ListNode(-1)  # Dummy head node
+        self.tail = self.head     # Initially, the tail is the same as the head
+
     
     def get(self, index: int) -> int:
-        curr = self.head.next
-        i = 0
+        curr = self.head.next  # Start from the first real node
+        i = 0                  # Index counter
         while curr:
             if i == index:
-                return curr.val
-            i += 1    
+                return curr.val  # Return the value if index is found
+            i += 1
             curr = curr.next
-        return -1
+        return -1  # Return -1 if index is out of bounds or list is empty
+
 
     def insertHead(self, val: int) -> None:
         new_node = ListNode(val)
-        new_node.next = self.head.next
-        self.head.next = new_node
-        #if the list was empty before inserting
-        if not new_node.next:
-            self.tail = new_node
+        new_node.next = self.head.next  # New node points to the first real node
+        self.head.next = new_node       # Head now points to the new node
+
+        if not new_node.next:  # If list was empty before insertion
+            self.tail = new_node  # Update tail to new node
+
 
     def insertTail(self, val: int) -> None:
-        self.tail.next = ListNode(val)
-        self.tail = self.tail.next
+        self.tail.next = ListNode(val)  # Create new node and link it to the tail
+        self.tail = self.tail.next      # Update tail to the new node
 
     def remove(self, index: int) -> bool:
-        i = 0 
-        curr = self.head
+        i = 0
+        curr = self.head  # Start from the dummy head
         while i < index and curr:
-            #Move curr to the node before the target node
             i += 1
-            curr = curr.next
+            curr = curr.next  # Traverse to the node before the one to be removed
+        
+        # Remove the node ahead of curr
         if curr and curr.next:
             if curr.next == self.tail:
-                self.tail = curr
-            curr.next = curr.next.next
+                self.tail = curr  # Update tail if last node is being removed
+            curr.next = curr.next.next  # Bypass the node to be removed
             return True
-        return False
+        return False  # Return False if index is out of bounds
+
 
 
     def getValues(self) -> List[int]:
-        curr = self.head.next
-        res = []
+        curr = self.head.next  # Start from the first real node
+        res = []  # List to store values
         while curr:
-            res.append(curr.val)
+            res.append(curr.val)  # Append each node's value to the list
             curr = curr.next
-        return res
+        return res  # Return the list of values
+
         
