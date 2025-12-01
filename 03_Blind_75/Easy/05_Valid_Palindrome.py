447077d691c23934c62b476f0cf4cdbfad440aee
Stephan Morris <stephancmorris@gmail.com>
2025-12-01 21:28:28 +1100

    Valid Palindrome

--- 03_Blind_75/Easy/05_Valid_Palindrome.py
+++ 03_Blind_75/Easy/05_Valid_Palindrome.py
@@ -22,9 +22,41 @@ def isPalindrome(s):
         Boolean indicating if string is palindrome
     """
     # TODO: Implement your solution here
+    left, right = 0, len(s) - 1
+    while left < right:
+        if s[left] != s[right]:
+            return False 
+        left += 1
+        right -= 1
+    return True
     pass
 
+    #Possible Solutions:
+    # 1. Two Pointers
+    # 2. Recursion
+    # 3. Iterative
+    # 4. Dynamic Programming
+    # 5. Backtracking
+    # 6. Greedy
+    # 7. Brute Force
+    # 8. Divide and Conquer
+    # 9. Bit Manipulatio
 
+# Solution Sliding Window
+# Space and time complexity: O(n)
+def isPalindrome(s):
+    left, right = 0, len(s) - 1
+    while left < right:
+        if s[left] != s[right]:
+            return False
+        left += 1
+        right -= 1
+    return True
+    
+    #Solution accouting for spaces and punctuation
+
+
+    
 # Test cases
 def test_valid_palindrome():
     """Test cases for Valid Palindrome"""
